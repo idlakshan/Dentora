f9372b98beee964f99aa5c27744b13e9b0032006
idlakshan <id.lakshan21@gmail.com>
2026-02-01 11:43:17 +0530

    feat: add admin page

---
+++ src/hooks/use-doctors.ts
@@ -0,0 +1,44 @@
+"use client";
+
+import {
+  createDoctor,
+  getDoctors,
+  updateDoctor,
+} from "@/lib/actions/doctors";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+export function useGetDoctors() {
+  const result = useQuery({
+    queryKey: ["getDoctors"],
+    queryFn: getDoctors,
+  });
+
+  return result;
+}
+
+export function useCreateDoctor() {
+  const queryClient = useQueryClient();
+
+  const result = useMutation({
+    mutationFn: createDoctor,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["getDoctors"] });
+    },
+    onError: (error) => console.log("Error while creating a doctor:", error),
+  });
+
+  return result;
+}
+
+export function useUpdateDoctor() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateDoctor,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["getDoctors"] });
+      queryClient.invalidateQueries({ queryKey: ["getAvailableDoctors"] });
+    },
+    onError: (error) => console.error("Failed to update doctor:", error),
+  });
+}
